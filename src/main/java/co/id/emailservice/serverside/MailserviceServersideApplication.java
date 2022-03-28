900b53198266e470d1c57f88618197e9b9b123d9
Dean Christiananda <alfadean60@gmail.com>
2022-03-28 12:17:48 +0900

    Inisiasi project email service - serverside

---
+++ src/main/java/co/id/emailservice/serverside/MailserviceServersideApplication.java
@@ -0,0 +1,13 @@
+package co.id.emailservice.serverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailserviceServersideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailserviceServersideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/emailservice/serverside/MailserviceServersideApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.emailservice.serverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailserviceServersideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
